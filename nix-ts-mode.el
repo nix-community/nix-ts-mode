6a1caee75615ae1f3ab16dbf3c68b4876247b2bd
Remi Gelinas <mail@remigelin.as>
2023-07-02 19:41:13 -0400

    feat: add more queries and initial indents

--- nix-ts-mode.el
+++ nix-ts-mode.el
@@ -15,71 +15,18 @@
 
 ;;; Code:
 
+(if (version< emacs-version "29.1")
+    (error "`nix-ts-mode` requires at least Emacs 29.1 for tree-sitter support."))
+
 (require 'treesit)
 
+(unless (treesit-available-p)
+  (error "`nix-ts-mode` requires Emacs to be built with tree-sitter support."))
+
+(unless (treesit-language-available-p 'nix)
+  (error "Nix tree-sitter grammar is not installed or is unable to be found."))
+
 (declare-function treesit-parser-create "treesit.c")
-
-;; Custom faces
-
-(defgroup nix-ts-font-faces nil
-  "Nix font faces."
-  :group 'nix-ts-mode
-  :group 'faces)
-
-(defface nix-ts-keyword-face
-  '((t :inherit font-lock-keyword-face))
-  "Used to highlight Nix keywords."
-  :group 'nix-ts-font-faces)
-
-(defface nix-ts-keyword-exception-face
-  '((t :inherit nix-ts-keyword-face))
-  "Used to highlight Nix exception keywords."
-  :group 'nix-ts-font-faces)
-
-(defface nix-ts-string-face
-  '((t :inherit font-lock-string-face))
-  "Used to highlight Nix strings."
-  :group 'nix-ts-font-faces)
-
-(defface nix-ts-string-interpolation-face
-  '((t :inherit font-lock-warning-face))
-  "Used to highlight interpolation in Nix strings."
-  :group 'nix-ts-font-faces)
-
-(defface nix-ts-path-face
-  '((t :inherit nix-ts-string-face))
-  "Used to highlight Nix paths."
-  :group 'nix-ts-font-faces)
-
-(defface nix-ts-constant-face
-  '((t :inherit font-lock-constant-face))
-  "Used to highlight Nix constants and literals."
-  :group 'nix-ts-font-faces)
-
-(defface nix-ts-operator-face
-  '((t :inherit font-lock-operator-face))
-  "Used to highlight Nix operators."
-  :group 'nix-ts-font-faces)
-
-(defface nix-ts-delimiter-face
-  '((t :inherit nix-ts-operator-face))
-  "Used to highlight Nix delimiters."
-  :group 'nix-ts-font-faces)
-
-(defface nix-ts-comment-face
-  '((t :inherit font-lock-comment-face))
-  "Used to highlight Nix comments."
-  :group 'nix-ts-font-faces)
-
-(defface nix-ts-bracket-face
-  '((t :inherit font-lock-bracket-face))
-  "Used to highlight Nix brackets."
-  :group 'nix-ts-font-faces)
-
-(defface nix-ts-builtin-face
-  '((t :inherit font-lock-builtin-face))
-  "Used to highlight Nix builtins."
-  :group 'nix-ts-font-faces)
 
 ;; Other
 
@@ -91,11 +38,11 @@
   :group 'nix)
 
 (defvar nix--treesit-builtins
-					; nix eval --impure --expr 'with builtins; filter (x: !(elem x [ "abort" "derivation" "import" "throw" ]) && isFunction builtins.${x}) (attrNames builtins)'
+  ;; nix eval --impure --expr 'with builtins; filter (x: !(elem x [ "abort" "derivation" "import" "throw" ]) && isFunction builtins.${x}) (attrNames builtins)'
   '("add" "addErrorContext" "all" "any" "appendContext" "attrNames" "attrValues" "baseNameOf" "bitAnd" "bitOr" "bitXor" "break" "catAttrs" "ceil" "compareVersions" "concatLists" "concatMap" "concatStringsSep" "deepSeq" "derivationStrict" "dirOf" "div" "elem" "elemAt" "fetchGit" "fetchMercurial" "fetchTarball" "fetchTree" "fetchurl" "filter" "filterSource" "findFile" "floor" "foldl'" "fromJSON" "fromTOML" "functionArgs" "genList" "genericClosure" "getAttr" "getContext" "getEnv" "getFlake" "groupBy" "hasAttr" "hasContext" "hashFile" "hashString" "head" "intersectAttrs" "isAttrs" "isBool" "isFloat" "isFunction" "isInt" "isList" "isNull" "isPath" "isString" "length" "lessThan" "listToAttrs" "map" "mapAttrs" "match" "mul" "parseDrvName" "partition" "path" "pathExists" "placeholder" "readDir" "readFile" "removeAttrs" "replaceStrings" "scopedImport" "seq" "sort" "split" "splitVersion" "storePath" "stringLength" "sub" "substring" "tail" "toFile" "toJSON" "toPath" "toString" "toXML" "trace" "traceVerbose" "tryEval" "typeOf" "unsafeDiscardOutputDependency" "unsafeDiscardStringContext" "unsafeGetAttrPos" "zipAttrsWith"))
 
 (defvar nix--treesit-constants
-					; nix eval --impure --expr 'with builtins; filter (x: !(isFunction builtins.${x} || isBool builtins.${x})) (attrNames builtins)'
+  ;; nix eval --impure --expr 'with builtins; filter (x: !(isFunction builtins.${x} || isBool builtins.${x})) (attrNames builtins)'
   '("builtins" "currentSystem" "currentTime" "langVersion" "nixPath" "nixVersion" "null" "storeDir"))
 
 ;; Settings
@@ -103,76 +50,107 @@
   (treesit-font-lock-rules
    :language 'nix
    :feature 'bracket
-   '(["(" ")" "[" "]" "{" "}"] @nix-ts-bracket-face)
+   '(["(" ")" "[" "]" "{" "}"] @font-lock-bracket-face)
 
    :language 'nix
    :feature 'comment
-   '((comment) @nix-ts-comment-face)
+   '((comment) @font-lock-comment-face)
 
    :language 'nix
    :feature 'delimiter
-   '(["," "." ";"] @nix-ts-delimiter-face)
+   '(["," "." ";"] @font-lock-delimiter-face)
 
    :language 'nix
    :feature 'keyword
    `((let_expression
-      (["let" "in"] @nix-ts-keyword-face))
+      (["let" "in"] @font-lock-keyword-face))
      (if_expression
-      ["if" "then" "else"] @nix-ts-keyword-face)
+      ["if" "then" "else"] @font-lock-keyword-face)
      (rec_attrset_expression
-      ("rec" @nix-keyword-face))
+      ("rec" @font-lock-keyword-face))
      (with_expression
-      ("with" @nix-keyword-face))
+      ("with" @font-lock-keyword-face))
      (assert_expression
-      ("assert" @nix-ts-keyword-exception-face))
-     ((identifier) @nix-ts-keyword-exception-face
+      ("assert" @font-lock-keyword-face))
+     ((identifier) @font-lock-keyword-face
       (:match
        ,(rx-to-string
 	 `(seq bol (or "throw" "abort")
 	       eol))
-       @nix-ts-keyword-exception-face)))
+       @font-lock-keyword-face))
+
+     ;; "or" is technically an operator, but we fontify it as a keyword
+     ((identifier) @font-lock-keyword-face
+      (:match
+       ,(rx-to-string
+	 `(seq bol "or" eol))
+       @font-lock-keyword-face)))
 
    :language 'nix
    :feature 'string
    :override t
-   `((string_fragment) @nix-ts-string-face
-     (string_expression ("\"" @nix-ts-string-face))
-     (indented_string_expression ("''" @nix-ts-string-face))
+   `((string_fragment) @font-lock-string-face
+     (string_expression
+      ("\"" @font-lock-string-face))
+     (indented_string_expression
+      ("''" @font-lock-string-face))
      (interpolation
-      ("${" @nix-ts-string-interpolation-face)
-      ("}" @nix-ts-string-interpolation-face)))
+      (["${" "}"] @font-lock-misc-punctuation-face)))
 
    :language 'nix
    :feature 'operator
-   `((binary_expression operator: _ @nix-ts-operator-face)
-     (unary_expression operator: _ @nix-ts-operator-face))
+   `((binary_expression operator: _ @font-lock-operator-face)
+     (unary_expression operator: _ @font-lock-operator-face))
 
    :language 'nix
    :feature 'number
-   `([(integer_expression) (float_expression)] @nix-ts-constant-face)
+   `([(integer_expression) (float_expression)] @font-lock-constant-face)
 
    :language 'nix
    :feature 'path
-   `((path_expression (path_fragment) @nix-ts-constant-face))
+   `((path_expression
+      (path_fragment) @font-lock-string-face))
 
    :language 'nix
    :feature 'builtin
-   `((variable_expression name: (identifier) @nix-ts-builtin-face
+   `((variable_expression name: (identifier) @font-lock-builtin-face
 			  (:match
 			   ,(rx-to-string
 			     `(seq bol (or ,@nix--treesit-builtins)
 				   eol))
-			   @nix-ts-builtin-face)))
+			   @font-lock-builtin-face)))
    :language 'nix
    :feature 'constant
-   `((variable_expression name: (identifier) @nix-ts-constant-face
+   `((variable_expression name: (identifier) @font-lock-constant-face
 			  (:match
 			   ,(rx-to-string
-			     `(seq bol (or ,@nix--treesit-constants)
+			     `(seq bol (or ,@nix--treesit-constants "true" "false")
 				   eol))
-			   @nix-ts-constant-face))))
+			   @font-lock-constant-face)))
+   :language 'nix
+   :feature 'attribute
+   `((attrpath
+      (identifier) @font-lock-variable-name-face))
+
+   :language 'nix
+   :feature 'ellipses
+   `((ellipses) @font-lock-misc-punctuation-face)
+
+   :language 'nix
+   :feature 'function
+   `((function_expression
+      ":" @font-lock-misc-punctuation-face)))
 
   "Tree-sitter font-lock settings for `nix-ts-mode'.")
+
+;; Indentation
+(defvar nix-ts-mode-indent-rules
+  (let ((offset nix-ts-mode-indent-offset))
+    `((nix
+       ((node-is "}") parent-bol 0)
+       ((node-is ")") parent-bol 0)
+       ((node-is "]") parent-bol 0)
+       ((parent-is "parenthesized_expression") parent-bol ,offset)))))
 
 ;;;###autoload
 (define-derived-mode nix-ts-mode prog-mode "Nix"
@@ -184,12 +162,17 @@
 
     ;; Font locking
     (setq-local treesit-font-lock-settings nix-ts-mode--font-lock-settings)
+    
     (setq-local treesit-font-lock-feature-list
                 '((comment builtin)
                   (keyword string path)
-                  (number constant)
-                  (bracket delimiter operator)))
+                  (number constant attribute)
+                  (bracket delimiter operator ellipses function)))
+    
     (setq-local treesit-font-lock-level 4)
+
+    ;; Indent
+    (setq-local treesit-simple-indent-rules nix-ts-mode-indent-rules)
     
     (treesit-major-mode-setup)))
 
