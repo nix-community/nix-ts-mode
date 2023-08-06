eb3cc4327a12647192434c04cacc8c06296a512a
Steve Purcell <steve@sanityinc.com>
2023-08-06 17:31:16 +0200

    Wrap commentary text

--- nix-ts-mode.el
+++ nix-ts-mode.el
@@ -23,8 +23,8 @@
 
 ;;; Commentary:
 
-
-;; A major mode for editing Nix expressions, powered by the new built-in tree-sitter support in Emacs 29.1.
+;; A major mode for editing Nix expressions, powered by the new
+;; built-in tree-sitter support in Emacs 29.1.
 
 ;;; Code:
 ;; (unless (version< emacs-version "29.1")
