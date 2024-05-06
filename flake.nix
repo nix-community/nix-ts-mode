1ddf0fb598310c421c4cdcc8db812d9ea85ab550
Remi Gelinas <mail@remigelin.as>
2024-05-06 15:07:53 -0400

    ci: lighten PR validation and change release process

--- flake.nix
+++ flake.nix
@@ -1,40 +1,44 @@
 {
   description = "An Emacs major mode for editing Nix expressions, powered by tree-sitter.";
 
-  outputs = {
-    systems,
-    nixpkgs-unstable,
-    pre-commit-nix,
-    ...
-  }: let
-    supportedSystems = import systems;
-    forAllSystems = nixpkgs-unstable.lib.genAttrs supportedSystems;
-  in {
-    supportedEmacsVersions = ["29.1" "snapshot" "release-snapshot"];
+  outputs =
+    { systems
+    , nixpkgs-unstable
+    , pre-commit-nix
+    , ...
+    }:
+    let
+      supportedSystems = import systems;
+      forAllSystems = nixpkgs-unstable.lib.genAttrs supportedSystems;
+    in
+    {
+      supportedEmacsVersions = [ "29.1" "snapshot" "release-snapshot" ];
 
-    devShells = forAllSystems (system: let
-      pre-commit-check = pre-commit-nix.lib.${system}.run {
-        src = ./.;
+      devShells = forAllSystems (system:
+        let
+          pre-commit-check = pre-commit-nix.lib.${system}.run {
+            src = ./.;
 
-        hooks = {
-          alejandra.enable = true;
-          deadnix.enable = true;
-          statix.enable = true;
-          commitizen.enable = true;
-        };
-      };
-    in {
-      default = nixpkgs-unstable.legacyPackages.${system}.mkShell {
-        name = "nix-ts-mode-shell";
+            hooks = {
+              deadnix.enable = true;
+              statix.enable = true;
+            };
+          };
+        in
+        {
+          default = nixpkgs-unstable.legacyPackages.${system}.mkShell {
+            name = "nix-ts-mode-shell";
 
-        packages = with nixpkgs-unstable.legacyPackages.${system}; [cask python311];
+            packages = with nixpkgs-unstable.legacyPackages.${system}; [ cask python311 ];
 
-        shellHook = ''
-          ${pre-commit-check.shellHook}
-        '';
-      };
-    });
-  };
+            shellHook = ''
+              ${pre-commit-check.shellHook}
+            '';
+          };
+        });
+
+      formatter = forAllSystems (system: nixpkgs-unstable.legacyPackages.${system}.nixpkgs-fmt);
+    };
 
   inputs = {
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
