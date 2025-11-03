b178206c6218466c8dd0a0c895f36cffd5284b63
Brian McGillion <bmg.avoin@gmail.com>
2025-11-03 10:17:32 +0400

    emacs-versions: add emacs 30.x for tests
    https://search.nixos.org/packages?channel=unstable&query=emacs
    
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- nix/emacs-versions.nix
+++ nix/emacs-versions.nix
@@ -1,2 +1,13 @@
 # Adjust to add new Emacs versions to CI
-[ "29.1" "29.2" "29.3" "snapshot" "release-snapshot" ]
+# Emacs 29.x support has been removed due to the use of new font-lock faces
+# Also https://search.nixos.org/packages?channel=25.05&query=emacs
+# shows that the lowest currently supported version of emacs in nixpkgs is 30.2
+[
+  "29.1"
+  "29.2"
+  "29.3"
+  "30.1"
+  "30.2"
+  "snapshot"
+  "release-snapshot"
+]
